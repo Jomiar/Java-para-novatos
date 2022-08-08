1db96d5da8a8b0cb876d6723f446b7f3e597faf8
Jose Dario Millan Aranibar <josedar1990@gmail.com>
2022-08-08 00:05:06 -0500

    Se creo la variable mi_array que tiene como elementos, variables de tipo String

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,6 +1,8 @@
 public class Metodos {
     public static void main(String[] args) {
         String mi_cadena = "Esto es una cadena de caracteres";
+        String[] mi_array = {mi_cadena, "Otra cadena"};
         System.out.println("La longitud de la cadena es: "+ mi_cadena.length());
+        System.out.println("La longitud del array es: " + mi_array.length);
     }
 }
