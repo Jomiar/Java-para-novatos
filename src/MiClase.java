daa98a08d40151d1a8911c1060a7d91b47a2943f
Jose Dario Millan Aranibar <josedar1990@gmail.com>
2022-08-08 15:37:30 -0500

    Se modifico los comentarios de la clase MyClase

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,14 +1,19 @@
 public class MiClase {
-    //private: signifca que no son accesibles desde fuera de la clase
-    //static: solo se creara una vez aunque se definan varios objetos de esta clase
+    /*  private: signifca que no son accesibles desde fuera de la clase
+    *   static: solo se creara una vez aunque se definan varios objetos
+    *   de esta clase */
     private static int miAtributo1;
     private float miAtributo2;
 
-    //Metodos public: Estos pueden ser usados por otros objetos externos a esta clase
-    //
+    /*  Metodos public: Estos pueden ser usados por otros objetos
+        externos a esta clase
+
+    *   El primer metodono devuelve ningun resultado (void)*/
     public void miMetodo1(){
 
     }
+    /* El segundo metodo: Devuelve un valor entero (int), tambien
+     * tiene una variable local de tipo int llamada r*/
     public int miMetodo2(int a,b){
         int r;
 
