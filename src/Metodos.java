e6b5b348d05ef713ec82af46abc11fe8a612c8d2
Jose Dario Millan Aranibar <josedar1990@gmail.com>
2022-08-07 23:50:40 -0500

    Mi primer commit y creacion de la clase Metodo

---
+++ src/Metodos.java
@@ -0,0 +1,6 @@
+public class Metodos {
+    public static void main(String[] args) {
+        String mi_cadena = "Esto es una cadena de caracteres";
+        System.out.println("La longitud de la cadena es: "+ mi_cadena.length());
+    }
+}
