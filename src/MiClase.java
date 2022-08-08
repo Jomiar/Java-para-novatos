7a18119a3c26cdb9b702eaf82007cb69b0863c6b
Jose Dario Millan Aranibar <josedar1990@gmail.com>
2022-08-08 14:52:23 -0500

    Se Creo la clase MiClase que contiene 2 atributos, miatributo1 y miAtributo2

---
+++ src/MiClase.java
@@ -0,0 +1,13 @@
+public class MiClase {
+    private static int miAtributo1;
+    private float miAtributo2;
+    
+    public void miMetodo(){
+
+    }
+    public int miMetodo2(int a,b){
+        int r;
+
+        return r;
+    }
+}
