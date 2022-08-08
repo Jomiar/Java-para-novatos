ee6cf092d4be3860ac0548b16344f0ba840ab3f5
Jose Dario Millan Aranibar <josedar1990@gmail.com>
2022-08-08 15:30:14 -0500

    Se agrego comentarios sobre los los terminos static, public y void

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,8 +1,12 @@
 public class MiClase {
+    //private: signifca que no son accesibles desde fuera de la clase
+    //static: solo se creara una vez aunque se definan varios objetos de esta clase
     private static int miAtributo1;
     private float miAtributo2;
-    
-    public void miMetodo(){
+
+    //Metodos public: Estos pueden ser usados por otros objetos externos a esta clase
+    //
+    public void miMetodo1(){
 
     }
     public int miMetodo2(int a,b){
